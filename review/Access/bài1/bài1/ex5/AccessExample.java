9a2b375aee5eb5313a4d00737ad1e06e997588c8
nguyenhieuzzz <24100145@st.phenikaa-uni.edu.vn>
2025-07-21 04:35:23 +0000

    h capnhat

--- review/Access/bài1/bài1/ex5/AccessExample.java
+++ review/Access/bài1/bài1/ex5/AccessExample.java
@@ -1,15 +1,17 @@
 public class AccessExample {
 
+    // Phương thức công khai, có thể truy cập từ bất kỳ đâu
     public void publicMethod() {
         System.out.println("Đây là phương thức công khai (truy cập từ bất cứ đâu)");
     }
 
+    // Phương thức private, chỉ có thể truy cập trong cùng một class
     private void privateMethod() {
         System.out.println("Đây là phương thức bí mật (chỉ truy cập ở cùng một class)");
     }
 
+    // Phương thức protected, chỉ có thể truy cập khi trong cùng package hoặc subclass
     protected void protectedMethod() {
         System.out.println("Đây là phương thức được protected (chỉ truy cập khi cùng package hoặc subclass)");
     }
 }
-
