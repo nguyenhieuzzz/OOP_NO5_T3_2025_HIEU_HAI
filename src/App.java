4c3348855d7c8e4387bc2458775879138cbd0ab2
nguyenhieuzzz <24100145@st.phenikaa-uni.edu.vn>
2025-07-22 10:05:44 +0000

    h capnhat

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 public class App {
-    // ... main method ...
-
-    public static void test() { // <-- Thay đổi thành public
-        System.out.println("sản phẩm");
-        // ... code test ...
+    
+    public static void main(String [] args){
+    QuanCaPhe.test();
     }
-}
+    }
+   
+
